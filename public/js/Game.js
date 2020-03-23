9e9560640aac078e9575fdaf868c51cd6cb2f861
Esteban Retana <netherblade619@gmail.com>
2020-03-22 20:35:06 -0600

    first commit

---
+++ public/js/Game.js
@@ -0,0 +1,34 @@
+//setup
+/*
+    create canvas
+    create boundaries
+    create character
+    create score
+    create obstacles
+    create eventlisteners (space and enter)
+*/
+//start 
+
+//update
+/*
+    update obstacles
+    update score
+    update character
+
+*/
+//menu
+//ui
+/*
+    start game option
+    settings
+    --sound
+*/
+//sound settings
+/*
+    change volume based on slider position
+    --master
+    --fx
+    --bird
+*/
+
+
