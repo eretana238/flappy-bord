c7666294ae261894e5c0aae3c80f862a5db54d76
Esteban Retana <netherblade619@gmail.com>
2020-03-23 22:12:15 -0600

    added bird

--- public/js/Game.js
+++ public/js/Game.js
@@ -1,34 +1,42 @@
-//setup
-/*
-    create canvas
-    create boundaries
-    create character
-    create score
-    create obstacles
-    create eventlisteners (space and enter)
-*/
-//start 
+// get canvas and context
+const canvas = document.getElementById("cvs")
+const ctx = canvas.getContext("2d")
 
-//update
-/*
-    update obstacles
-    update score
-    update character
+// Frames
+let frames = 0
 
-*/
-//menu
-//ui
-/*
-    start game option
-    settings
-    --sound
-*/
-//sound settings
-/*
-    change volume based on slider position
-    --master
-    --fx
-    --bird
-*/
+// Objects
+let bord = {
+    x: 10,
+    y: 50,
+    r: 10,
+    draw: function() {
+        ctx.fillStyle = "#fff"
+        ctx.ellipse(this.x, this.y, this.r, this.r,0,0,2* Math.PI)
+        ctx.fill()
+    },
+    update: function() {
 
+    }
+}
 
+function update() {
+    
+}
+
+function draw() {
+    ctx.fillStyle = "#333"
+    ctx.fillRect(0,0,canvas.width, canvas.height)
+
+    bord.draw()
+    
+}
+
+function loop() {
+    update()
+    draw()
+    frames++
+    requestAnimationFrame(loop)
+}
+
+loop()
