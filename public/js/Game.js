2ff4dee1cab6b4b4f93d1f8a36a8c6dbd19e40e6
Esteban Retana <netherblade619@gmail.com>
2020-03-24 22:53:12 -0600

    added bord and warps

--- public/js/Game.js
+++ public/js/Game.js
@@ -2,18 +2,59 @@
 const canvas = document.getElementById("cvs")
 const ctx = canvas.getContext("2d")
 
+// Resize canvas to full screen
+canvas.width  = window.innerWidth
+canvas.height = window.innerHeight
+
 // Frames
 let frames = 0
 
+// Event listener
+canvas.addEventListener("click", () =>{
+    bord.jump()
+})
+// canvas.onkeyup = function(e) {
+//     if(e.keyCode == 32) {
+//         bord.jump()
+//     }
+// }
+
 // Objects
 let bord = {
-    x: 10,
+    x: 50,
     y: 50,
-    r: 10,
+    r: 20,
+    gravity: 0.25,
+    speed: 0,
+    draw: function() {
+        ctx.save()
+        ctx.fillStyle = "#fff"
+        ctx.ellipse(this.x, this.y, this.r, this.r, 0, 0, 2 * Math.PI)
+        ctx.fill()
+        ctx.restore()
+    },
+    update: function() {
+        this.speed += this.gravity 
+        this.y += this.speed
+    },
+    jump: function() {
+        this.speed -= 4.5
+    }
+}
+
+let warp = {
+    gap: 130,
+    x: 100,
+    y: 0,
+    h: 400,
+    w: 50,
     draw: function() {
         ctx.fillStyle = "#fff"
-        ctx.ellipse(this.x, this.y, this.r, this.r,0,0,2* Math.PI)
-        ctx.fill()
+        ctx.fillRect(this.x, this.y, this.w, this.h)
+
+        ctx.fillStyle = "#fff"
+        ctx.fillRect(this.x, this.y + this.h + this.gap, this.w, this.h)
+
     },
     update: function() {
 
@@ -21,7 +62,7 @@ let bord = {
 }
 
 function update() {
-    
+    bord.update()
 }
 
 function draw() {
@@ -29,7 +70,7 @@ function draw() {
     ctx.fillRect(0,0,canvas.width, canvas.height)
 
     bord.draw()
-    
+    warp.draw()
 }
 
 function loop() {
