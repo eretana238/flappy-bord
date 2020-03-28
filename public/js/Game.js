741e307db1769094606f5a844c0486ddae31aaf9
Esteban Retana <netherblade619@gmail.com>
2020-03-27 22:46:22 -0600

    fixed collisions

--- public/js/Game.js
+++ public/js/Game.js
@@ -9,10 +9,13 @@ canvas.height = window.innerHeight
 // Frames
 let frames = 0
 
+// States
+let gameState = 1                           /* 0 == endgame, 1 == running, 2 == pause */
+
 // Objects
 let bord = {
     x: 50,
-    y: 150,
+    y: canvas.height/2,
     r: 20,
     gravity: 0.3,
     speed: 0,
@@ -23,11 +26,10 @@ let bord = {
         ctx.fill()
         ctx.closePath()
     },
-
     // increases speed of the bird downwards
     update: function() {
-        if(this.y > canvas.height - 25){
-            this.resetSpeed()
+        if(this.y > canvas.height - this.r) {
+            gameState = 0
         }else{
             this.speed += this.gravity 
             this.y += this.speed
@@ -35,21 +37,17 @@ let bord = {
     },
     jump: function() {
         this.speed = -7.0
-    },
-
-    resetSpeed: function(){
-        this.y = canvas.height - 25
-        this.speed = 0
     }
 }
 
 let warps = {
     position: [],
-    gap: 150,
-    maxY: -300,
+    gap: 120,
+    maxY: -380,
+    minY: 0,
     h: 500,
     w: 50,
-    dx: 2,
+    dx: 3,
     draw: function() {
         for(let i = 0; i < this.position.length; i++) {
             let p = this.position[i]
@@ -64,10 +62,10 @@ let warps = {
         }
     },
     update: function() {
-        if(frames % 150 == 0) {
+        if(frames % 100 == 0) {
             this.position.push({
                 x: canvas.width,
-                y: this.maxY * (Math.random() + 1)
+                y: Math.random() * this.maxY
             })
         }
         for(let i = 0; i < this.position.length; i++) {
@@ -86,9 +84,41 @@ document.addEventListener("keydown", action)
 
 function action(e) {
     switch(e.keyCode) {
-        case 32:
-            bord.jump()
+        case 32:                            /* space bar = bord jump*/
+            bord.jump()  
             break
+        case 13:                            /* enter = restart game*/
+            if(gameState == 0) {
+                gameState = 1
+                // reset frames
+                frames = 0
+                // Reset bord
+                bord.y = 150
+                bord.speed = 0
+
+                // Reset warps
+                warps.position = []
+            }
+            break
+        case 27:                            /* escape (pause) */
+            // Toggle states
+            if(gameState == 1)
+                gameState = 2
+            else if(gameState == 2)
+                gameState = 1
+    }
+}
+
+function hasCollided() {
+    for(let i = 0; i < warps.position.length; i++) {
+        let p = warps.position[i]
+        // check top and botom warps inside x boundaries
+        if(bord.x >= p.x && bord.x <= p.x + warps.w && (bord.y - bord.r <= p.y + warps.h || bord.y + bord.r >= p.y + warps.h + warps.gap)) 
+            return true
+
+        if(bord.x + bord.r >= p.x && bord.x - bord.r <= p.x && (bord.y < p.y + warps.h || bord.y > p.y + warps.h + warps.gap))
+            return true
+
     }
 }
 
@@ -105,9 +135,13 @@ function draw() {
 }
 
 function loop() {
-    update()
-    draw()
-    frames++
+    if(gameState == 1) {
+        update()
+        draw()
+        frames++
+        if(hasCollided())
+            gameState = 0
+    }
     requestAnimationFrame(loop)
 }
 
